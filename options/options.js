95e45ba5636c2963e2f1628f0b2c5abbca39e7b7
Crowdbotics <team@crowdbotics.com>
2023-07-30 02:49:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-waterfall-42519",
+  url: "https://billowing-waterfall-42519.botics.co",
+  api: "https://billowing-waterfall-42519.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
